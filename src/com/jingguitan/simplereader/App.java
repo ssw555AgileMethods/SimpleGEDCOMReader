9bbc1d28bb6f61c040a5c234657edd5ca984a745
baozijianke <tanjinggui@gmail.com>
2018-02-19 19:11:09 -0500

    Revert "Merge pull request #1 from ssw555AgileMethods/Test"
    This reverts commit ce45e5e22904cc9fd9a672e46736c6c77432ca39, reversing
    changes made to 23572e096f4b108ac3e4cdcb99a1094590c311a2.

--- src/com/jingguitan/simplereader/App.java
+++ src/com/jingguitan/simplereader/App.java
@@ -3,6 +3,6 @@ package com.jingguitan.simplereader;
 public class App {
 	public static void main(String[] args) {
 		FileReader fd = new FileReader();
-		fd.readFrom("input.ged");
+		fd.readFrom("export-Forest.ged");
 	}
 }

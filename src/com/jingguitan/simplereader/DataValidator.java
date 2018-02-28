d1d60a3089232679be09959f68cc651ab39c2011
Jinggui Tan <tanjinggui@gmail.com>
2018-02-28 16:39:23 -0500

    US03 US04 finished
    Function of US03 US04 has been finished.

--- src/com/jingguitan/simplereader/DataValidator.java
+++ src/com/jingguitan/simplereader/DataValidator.java
@@ -11,16 +11,53 @@ package com.jingguitan.simplereader;
 public class DataValidator {
 	
 	// US03 Birth before death
-	public boolean birtBeforeDeat() {
-		
-		
-		return false;
+	public boolean birtBeforeDeat(Individual indi) {
+		//may not die
+		if(indi.getAlive().equals("True")) {
+			return true;
+		}
+		return this.compareDate(indi.getBirthday(), indi.getDeathday());
 	}
 	
 	// US04 Marriage before divorce
-	public boolean birtBeforeDivo() {
+	public boolean birtBeforeDivo(Family fam) {
 		
+		//may not divorce
+		if(fam.getDivorced().equals("NA")) {
+			return true;
+		}
+		return this.compareDate(fam.getMarried(), fam.getDivorced());
+	}
+	
+	private boolean compareDate(String date1, String date2) {
+		int year1 = Integer.parseInt(date1.substring(0, 4));
+		int month1 = Integer.parseInt(date1.substring(5, 7));
+		int day1 = Integer.parseInt(date1.substring(9));
 		
+		int year2 = Integer.parseInt(date2.substring(0, 4));
+		int month2 = Integer.parseInt(date2.substring(5, 7));
+		int day2 = Integer.parseInt(date2.substring(9));
+		
+		if(year1 < year2) {
+			return true;
+		}else if(year1 > year2){
+			return false;
+		}else if(year1 == year2){
+			if(month1 < month2) {
+				return true;
+			}else if(month1 > month2) {
+				return false;
+			}else if(month1 == month2) {
+				if(day1 < day2) {
+					return true;
+				}else if(day1 > day2) {
+					return false;
+				}else if(day1 == day2) {
+					// birth and die in the same day or marry and divorce in the same day
+					return true;
+				}
+			}
+		}
 		return false;
 	}
 }
